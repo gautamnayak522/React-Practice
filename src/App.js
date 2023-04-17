b58dfbe53834891d3d9c2ce6a5767939d87a6938
gautam.nayak <gautam.nayak@radixweb.com>
2023-04-17 13:08:58 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Employee from './Components/Employee';
+import Department from './Components/Department';
 
 function App() {
+
+  const data = "aaaaa";
+  var isLoggedIn = true;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoggedIn && <MyButton/>}
+      {data}
+      
+      <hr/>
+
+      <Employee/>
+      <Department/>
+
     </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
   );
 }
 
---
+++ src/Components/Department.js
@@ -0,0 +1,9 @@
+function Department() {
+    return ( 
+        <div>
+            <h2>Department</h2>
+        </div>
+     );
+}
+
+export default Department;
